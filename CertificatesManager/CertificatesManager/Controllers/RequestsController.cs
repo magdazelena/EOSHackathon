ec1cd3553ac8a9dcdae692352c9ef6d8a8e4701c
Filip Nizioł <filip.niziol@gmail.com>
2018-06-09 19:19:35 +0800

    Start working with Requests

--- CertificatesManager/CertificatesManager/Controllers/RequestsController.cs
+++ CertificatesManager/CertificatesManager/Controllers/RequestsController.cs
@@ -15,13 +15,15 @@ namespace CertificatesManager.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Requests
+        [Authorize]
         public ActionResult Index()
         {
             return View(db.Requests.ToList());
         }
 
         // GET: Requests/Details/5
-        public ActionResult Details(int? id)
+        [Authorize]
+        public ActionResult Verify(int? id)
         {
             if (id == null)
             {
@@ -33,12 +35,6 @@ namespace CertificatesManager.Controllers
                 return HttpNotFound();
             }
             return View(request);
-        }
-
-        // GET: Requests/Create
-        public ActionResult Create()
-        {
-            return View();
         }
 
         // POST: Requests/Create
@@ -62,38 +58,8 @@ namespace CertificatesManager.Controllers
             return new HttpStatusCodeResult(HttpStatusCode.NotFound);
         }
 
-        // GET: Requests/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Request request = db.Requests.Find(id);
-            if (request == null)
-            {
-                return HttpNotFound();
-            }
-            return View(request);
-        }
-
-        // POST: Requests/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,EOSRequestorName,CertificateId,Email,Status")] Request request)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(request).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(request);
-        }
-
         // GET: Requests/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -111,6 +77,7 @@ namespace CertificatesManager.Controllers
         // POST: Requests/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult DeleteConfirmed(int id)
         {
             Request request = db.Requests.Find(id);
