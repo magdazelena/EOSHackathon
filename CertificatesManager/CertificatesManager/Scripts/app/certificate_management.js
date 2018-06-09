46ae41bc9f2f63716b0244e8b79a42d42040a425
magda.zelezik <magda,zelezik@gmail.com>
2018-06-09 22:19:17 +0800

    DOM fields to cert ajax

--- CertificatesManager/CertificatesManager/Scripts/app/certificate_management.js
+++ CertificatesManager/CertificatesManager/Scripts/app/certificate_management.js
@@ -6,8 +6,14 @@
             url: $(this).attr("action"),
             data: $(this).serialize(),
             success: function (data) { //call successfull
+                
+                var ecc = eosjs_ecc;
+
                 var certificateId = data.certificateId;
-                alert(certificateId);
+                var name = $("#Name").val(); 
+                var placeofissue = $("#PlaceOfIssue").val();
+                var content = $("#Content").val();
+
                 /*
 
                 eos = Eos({ keyProvider: '5JSYsDkyCp3p7zsMcE2Sv7Ep3gii6Vm2wL4ED4dW5j2XQSFm38S', httpEndpoint: 'http://eoshackathon.eastasia.cloudapp.azure.com:8888' })
