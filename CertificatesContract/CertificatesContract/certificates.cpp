1560e094c5c00cb94a2250b9aa0df1fe445820f4
Filip Nizioł <filip.niziol@gmail.com>
2018-06-09 13:22:13 +0800

    certificates.cpp - full contract

--- CertificatesContract/CertificatesContract/certificates.cpp
+++ CertificatesContract/CertificatesContract/certificates.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class certificates : public eosio::contract {
 public:
 	certificates(action_name self) : contract(self)
-		, _certificate(_self, _self)
+		, _certificates(_self, _self)
 		, _requests {}
 	
 	//@abi table 
@@ -31,39 +31,62 @@ public:
 	multi_index<N(request), request> _requests;
 
 	//@abi action
-	void addcertificate(const account_name account, string& username) {
-		/**
-		* We require that only the owner of an account can use this action
-		* or somebody with the account authorization
-		*/
-		require_auth(account);
+	void addcertificate(uint64_t certificateId,	string certificateHash,	name issuer) {
+		require_auth(issuer);
 
-		/**
-		* We access the "player" table as creating an object of type "playerIndex"
-		* As parameters we pass code & scope - _self from the parent contract
-		*/
-		playerIndex players(_self, _self);
+		auto iterator = _certificates.find(certificateId);
+		eosio_assert(iterator == _certificates.end(), "Certificate with given Id already exists");
 
-		/**
-		* We must verify that the account doesn't exist yet
-		* If the account is not found the iterator variable should be players.end()
-		*/
-		auto iterator = players.find(account);
-		eosio_assert(iterator == players.end(), "Address for account already exists");
-
-		/**
-		* We add the new player in the table
-		* The first argument is the payer of the storage which will store the data
-		*/
-		players.emplace(account, [&](auto& player) {
-			player.account_name = account;
-			player.username = username;
-			player.level = 1;
-			player.health_points = 1000;
-			player.energy_points = 1000;
+		_certificates.emplace(issuer, [&](auto& row) {
+			row.certificateId = certificateId;
+			row.certificateHash = certificateHash;
+			row.issuer = issuer;
 		});
+
+		print("Certificate added");
 	}
 
+	//@abi action
+	void delcertificate(uint64_t certificateId, name issuer) {
+		require_auth(issuer);
+
+		auto iterator = _certificates.find(certificateId);
+		eosio_assert(iterator != _certificates.end(), "Certificate with given Id does not exist");
+
+		_certificates.erase(iterator);
+		
+		print("Certificate deleted");
+
+	}
+
+	//@abi action
+	void addrequest(uint64_t requestId,	name requestor,	string email) {
+		require_auth(requestor);
+
+		auto iterator = _requests.find(requestId);
+		eosio_assert(iterator == _requests.end(), "Request with given Id already exists");
+
+		_requests.emplace(requestor, [&](auto& row) {
+			row.requestId = requestId;
+			row.requestor = requestor;
+			row.email = email;
+		});
+
+		print("Request added");
+	}
+
+	//@abi action
+	void delrequest(uint64_t requestId, name requestor) {
+		require_auth(requestor);
+
+		auto iterator = _requests.find(requestId);
+		eosio_assert(iterator != _requests.end(), "Request with given Id does not exist");
+
+		_requests.erase(iterator);
+
+		print("Request deleted");
+
+	}
 };
 
 // EOSIO_ABI(certificates, (addcertificate)(delcertificate)(addrequest)(delrequest))
