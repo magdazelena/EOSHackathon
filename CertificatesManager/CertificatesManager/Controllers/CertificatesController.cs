ba3527f40db53e0ee93ed006a36e62b14f5dd38b
Filip Nizioł <filip.niziol@gmail.com>
2018-06-09 23:34:19 +0800

    Changing delete to reject

--- CertificatesManager/CertificatesManager/Controllers/CertificatesController.cs
+++ CertificatesManager/CertificatesManager/Controllers/CertificatesController.cs
@@ -81,40 +81,9 @@ namespace CertificatesManager.Controllers
 
             return View(certificate);
         }
-
-        //// GET: Certificates/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Certificate certificate = db.Certificates.Find(id);
-        //    if (certificate == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(certificate);
-        //}
-
-        //// POST: Certificates/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "Id,Name,Author,PlaceOfIssue,Content,EOSAuthorAccount,EOSOwnerAccount")] Certificate certificate)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(certificate).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(certificate);
-        //}
-
-        // GET: Certificates/Delete/5
-        public ActionResult Delete(int? id)
+        
+        // GET: Certificates/Reject/5
+        public ActionResult Reject(int? id)
         {
             ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
 
@@ -131,10 +100,10 @@ namespace CertificatesManager.Controllers
             return View(certificate);
         }
 
-        // POST: Certificates/Delete/5
-        [HttpPost, ActionName("Delete")]
+        // POST: Certificates/Reject/5
+        [HttpPost, ActionName("Reject")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult RejectConfirmed(int id)
         {
             ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
 
