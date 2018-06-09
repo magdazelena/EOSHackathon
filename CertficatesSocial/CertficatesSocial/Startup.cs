e943f4e9dc5c1733622774b7130c6a88bad98737
magda.zelezik <magda,zelezik@gmail.com>
2018-06-09 11:21:34 +0800

    Created CertificatesSocial solution

---
+++ CertficatesSocial/CertficatesSocial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CertficatesSocial.Startup))]
+namespace CertficatesSocial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
