dff9a2ffd7d6f02d1f03a59790eb9f317894cdbb
Filip Nizioł <filip.niziol@gmail.com>
2018-06-09 14:10:14 +0800

    Updating requests.cpp and Certificate.cs, Requests.cs

---
+++ CertificatesManager/CertificatesManager/Models/Certificate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CertificatesManager.Models
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string PlaceOfIssue { get; set; }
+
+        public string Content { get; set; }
+
+        public string EOSAuthorAccount { get; set; }
+
+        public string EOSOwnerAccount { get; set; }
+    }
+}
---
+++ CertificatesManager/CertificatesManager/Models/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CertificatesManager.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+
+        public string EOSRequestorName { get; set; }
+
+        public string EOSRequestorName { get; set; }
+    }
+}
