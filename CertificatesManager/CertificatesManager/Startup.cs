dfb2b441b06370c823cc2a2100077ac0855740f7
magda.zelezik <magda,zelezik@gmail.com>
2018-06-09 11:25:36 +0800

    Cerficates manager solution created

---
+++ CertificatesManager/CertificatesManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CertificatesManager.Startup))]
+namespace CertificatesManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
