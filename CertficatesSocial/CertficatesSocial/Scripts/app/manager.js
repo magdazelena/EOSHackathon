880ede1eedf5c6ce5354fa67c855695dd11b6caa
magda.zelezik <magda,zelezik@gmail.com>
2018-06-09 17:07:00 +0800

    Certificates Social scatter integration and sending request ajax data

---
+++ CertficatesSocial/CertficatesSocial/Scripts/app/manager.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    var scatter;
+
+    document.addEventListener('scatterLoaded', scatterExtension => {
+        scatter = window.scatter;
+        window.scatter = null;
+    });
+
+    $(".confirmButton").click(function () {
+        var id = $(this).attr("id");
+
+
+        const requiredFields = {
+            personal: ['email'],
+            accounts: [
+                { blockchain: 'eos', host: 'eoshackathon.eastasia.cloudapp.azure.com', port: 8888 }
+            ]
+        };
+        const network = {
+            blockchain: 'eos',
+            host: 'eoshackathon.eastasia.cloudapp.azure.com',
+            port: 8888
+        }
+
+
+        scatter.getIdentity(requiredFields).then(identity => {
+            var email = identity.personal.email;
+            var eosname = identity.accounts[0].name;
+            $.ajax({
+                type: "POST",
+                url: 'https://certificatesmanager.azurewebsites.net/Requests/Create',
+                data: JSON.stringify({ "EOSRequestorName": eosname, "CertificateId": id, "Email" : email }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data) { //call successfull
+                    var requestId = data.requestId;
+                },
+                error: function (xhr) {
+                    //error occurred
+                }
+            });
+        }).catch(error => {
+            //...
+        });
+    });
+});
