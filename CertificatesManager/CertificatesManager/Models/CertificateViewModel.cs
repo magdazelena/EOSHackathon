1000731bd9252cddd76a336e25b294b67d833531
magda.zelezik <magda,zelezik@gmail.com>
2018-06-09 21:56:26 +0800

    Labels for certificate creation

--- CertificatesManager/CertificatesManager/Models/CertificateViewModel.cs
+++ CertificatesManager/CertificatesManager/Models/CertificateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,13 @@ namespace CertificatesManager.Models
 {
     public class CertificateViewModel
     {
+        [Display(Name = "Certificate name")]
         public string Name { get; set; }
-        
+
+        [Display(Name = "Place of issue")]
         public string PlaceOfIssue { get; set; }
 
+        [Display(Name = "Certificate contents")]
         public string Content { get; set; }
         
         public string EOSOwnerAccount { get; set; }
